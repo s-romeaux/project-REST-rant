d3edd2b74e6fafca9cf76244663fc9dd39aacfbe
S-Romeaux <sal.4845@gmail.com>
2023-11-02 23:26:20 -0700

    add'l entries

--- views/default.jsx
+++ views/default.jsx
@@ -4,7 +4,7 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>RaveRant</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
                 <link rel="stylesheet" href="/css/style.css"/>
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
