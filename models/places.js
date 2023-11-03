1e941f93c0e95b0c0cb0a20c6e06a9e0b0b60eb1
S-Romeaux <sal.4845@gmail.com>
2023-11-02 22:23:58 -0700

    Cleanup Capitals/Json

--- models/places.js
+++ models/places.js
@@ -16,27 +16,3 @@ placeSchema.methods.showEstablished = function() {
 }
 
 module.exports = mongoose.model('Place', placeSchema)
-
-
-
-
-
-
-
-// module.exports = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '/images/h-thai-ml-tables.jpg'
-// }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/coffee-cat.jpg'
-// }]
-
-
-
-
