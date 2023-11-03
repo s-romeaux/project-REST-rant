b4e1c0f58110aa4dcb55fa58f91485a15e3b050d
S-Romeaux <sal.4845@gmail.com>
2023-11-02 22:38:29 -0700

    Removed console log msg

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Modules and Globals
 require('dotenv').config()
-console.log('Testing console logging...')
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
